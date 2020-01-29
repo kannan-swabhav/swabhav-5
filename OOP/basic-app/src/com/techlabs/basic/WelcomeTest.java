7ba57a7eb19ca7a16d4d9e9f847d36b79a82d0ea
jayz <jayshelke84@gmail.com>
2020-01-29 07:48:09 +0530

    jan 29

--- OOP/basic-app/src/com/techlabs/basic/WelcomeTest.java
+++ OOP/basic-app/src/com/techlabs/basic/WelcomeTest.java
@@ -1,6 +1,7 @@
 package com.techlabs.basic;
 
 import java.awt.SystemColor;
+import java.util.Arrays;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
@@ -15,17 +16,32 @@ public class WelcomeTest {
 
 		printInfo("2");
 
+		String str = "http//swabhavtechlabs.com?developerispawan&roleisarchitect";
+		String[] SplitStr = str.split("http//|.com\\?developeris|&roleis");
+
+		for (String a : SplitStr) {
+			System.out.println(a);
+		}
+
 	}
 
 	public static void printInfo(String a) {
 		System.out.println(a);
 	}
-	
+
 	public static void printInfo(float a) {
 		System.out.println(a);
 	}
-	
+
 	public static void printInfo(int a) {
+		System.out.println(a);
+	}
+
+	public static void printInfo(char a) {
+		System.out.println(a);
+	}
+
+	public static void printInfo(Double a) {
 		System.out.println(a);
 	}
 
